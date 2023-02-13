f9a6a00520bbc8f866155ceb82e678811141c637
Shriram Patil <shrirampatil246@gmail.com>
2023-02-13 19:28:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your code here
-function myFunc() {
-	let insertedValue = document.getElementById("newTodoInput").value;
-	if(insertedValue != "") {
-		let orderedList = document.getElementsByTagName("ol")[0];
-		let createList = document.createElement("li");
-		createList.textContent = insertedValue;
-		orderedList.appendChild(createList);
-		document.getElementById("newTodoInput").value = "";
-	}
+function addtodo(){
+    let btn1 = document.getElementById("todoList")
+    
+    let btn = document.createElement('li')
+    btn.textContent="blue"
+
+    btn1.appendChild(btn)
+    
+       
 }
 
