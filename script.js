5f47c9a6dc356b2810e1dd6485cc8da35b183e91
Shriram Patil <shrirampatil246@gmail.com>
2023-02-16 09:07:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-//your code here
 var todoValue = document.getElementById("newTodoInput");
-var btn =document.getElementById("addTodoBtn");
+var btn = document.getElementById("addTodoBtn");
 var ol = document.getElementById("todoList");
 
-function addTodo(){
-	
-	if(todoValue.value){
-	var lii =document.createElement('li');
-			lii.textContent = todoValue.value;
-			todoValue.value= "";
-			ol.appendChild(li);
-		
-	}
+function addTodo() {
+  //alert(todoValue.value);
+  if (todoValue.value) {
+    // we need to create a li
+    var li = document.createElement("li");
+    li.textContent = todoValue.value;
+    ol.appendChild(li);
+    todoValue.value = "";
+  }
 }
-btn.addEventListener('click',addTodo);
+
+btn.addEventListener("click", addTodo);
