57d7a7a942da4c9ac272f1f3f0cddd08f0ec2b23
Shriram Patil <shrirampatil246@gmail.com>
2023-02-16 09:11:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var btn = document.getElementById("addTodoBtn");
 var ol = document.getElementById("todoList");
 
 function addTodo() {
-  //alert(todoValue.value);
+  
   if (todoValue.value) {
     // we need to create a li
     var li = document.createElement("li");
